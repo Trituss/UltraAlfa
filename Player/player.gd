4a9502a01f462bf180cd6c7e375a5eb6cf48c336
Trituss <estevanlopezibcc@gmail.com>
2023-11-05 17:15:07 -0600

    Agregado de movimiento

--- Player/player.gd
+++ Player/player.gd
@@ -2,13 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func _ready():
-	get_node("AnimatedSprite2D").play("Idle")
+@onready var anim = get_node("AnimationPlayer")
 	
 func _physics_process(delta):
 	# Add the gravity.
@@ -18,13 +16,24 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	print(direction)
+	if direction ==-1:
+			get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1:
+			get_node("AnimatedSprite2D").flip_h = false
+		
 	if direction:
 		velocity.x = direction * SPEED
+		if velocity.y ==0:
+			anim.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y ==0:
+			anim.play("Idle")
 
 	move_and_slide()
